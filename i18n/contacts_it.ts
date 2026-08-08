33cbee0ece286e24e291a6a89cda3b8a3ad50330
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:41 +0200

    feat: Add italian  translations

---
+++ i18n/contacts_it.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>Person</source>
+        <translation>Persona</translation>
+    </message>
+    <message>
+        <source>Organization</source>
+        <translation>Organizzazione</translation>
+    </message>
+    <message>
+        <source>Open in contacts app</source>
+        <translation>Apri nell&apos;app contatti</translation>
+    </message>
+    <message>
+        <source>Phone</source>
+        <translation>Telefono</translation>
+    </message>
+    <message>
+        <source>Copy phone number &apos;%1&apos;</source>
+        <translation>Copia numero di telefono &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>Call phone number &apos;%1&apos;</source>
+        <translation>Chiama il numero &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>iMessage to &apos;%1&apos;</source>
+        <translation>iMessage a &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <source>Copy email address &apos;%1&apos;</source>
+        <translation>Copia indirizzo email &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>Send mail to &apos;%1&apos;</source>
+        <translation>Invia email a &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>Website</source>
+        <translation>Sito web</translation>
+    </message>
+    <message>
+        <source>Copy website address &apos;%1&apos;</source>
+        <translation>Copia indirizzo del sito web &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>Open website &apos;%1&apos;</source>
+        <translation>Apri sito web &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>The application is not authorized to access contacts.</source>
+        <translation>L&apos;applicazione non è autorizzata ad accedere ai contatti.</translation>
+    </message>
+    <message>
+        <source>The user denied access to contacts.</source>
+        <translation>L&apos;utente ha negato l&apos;accesso ai contatti.</translation>
+    </message>
+</context>
+</TS>
